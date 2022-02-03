9543a81046e5f9777e29eeb38eaab77f340339cd
fanik05 <fatinanik129@gmail.com>
2022-02-03 17:16:08 +0600

    made router v6 to behave like v5 -_-

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,35 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Router, Routes, Route } from 'react-router-dom';
+import { useLayoutEffect, useState } from 'react';
 import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import history from '../history';
+
+const BrowserRouter = ({ history, ...props }) => {
+  const [state, setState] = useState({
+    action: history.action,
+    location: history.location,
+  });
+
+  useLayoutEffect(() => history.listen(setState), [history]);
+
+  return (
+    <Router
+      {...props}
+      location={state.location}
+      navigationType={state.action}
+      navigator={history}
+    />
+  );
+};
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <BrowserRouter history={history}>
         <Header />
         <Routes>
           <Route path="/" element={<StreamList />} />
